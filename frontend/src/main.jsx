d7a80a6cc29bb77528dc8eac5db2e3ef1aa2282e
twinkgenocide <illusivelove@proton.me>
2025-10-17 13:50:07 -0300

    Añadida boceto de página 'Blog' inicial

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LayoutPublic } from './layouts/LayoutPublic';
 import './index.css'
+import { Blog } from './pages/public/Blog/Blog';
 
 const router = createBrowserRouter([
-  { path: '/', element: <LayoutPublic /> }
+  {
+    path: '/',
+    element: <LayoutPublic />,
+    children: [
+      {
+        path: '/blogs/:blogId',
+        element: <Blog />
+      }
+    ]
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
