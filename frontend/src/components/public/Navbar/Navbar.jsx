de2521f36ad5d90edd3067b288e22c8365b881fa
twinkgenocide <illusivelove@proton.me>
2025-10-16 22:16:56 -0300

    Añadida clase .navbar a elemento nav en Navbar

--- frontend/src/components/public/Navbar/Navbar.jsx
+++ frontend/src/components/public/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 
 export function Navbar() {
     return <>
-        <nav>
+        <nav className="navbar">
             <div>
                 <Link className="sosdrink-logo" to="/">
                     <p>SOSDRINK</p>
