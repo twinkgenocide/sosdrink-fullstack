bf6475c6e00fa1ee42ee8c8acfc45bf67b188310
twinkgenocide <illusivelove@proton.me>
2025-10-17 12:47:05 -0300

    Añadido componente 'Disclaimer'

---
+++ frontend/src/components/public/Disclaimer/Disclaimer.jsx
@@ -0,0 +1,18 @@
+import "./Disclaimer.css"
+
+export function Disclaimer() {
+    return <>
+        <div className="box-disclaimer">
+            <h2>Aviso</h2>
+            <p>
+                El consumo de bebidas alcohólicas y productos de tabaco debe ser responsable y moderado.
+                No se recomienda su consumo para menores de 18 años. Las imágenes y descripciones tienen
+                fines informativos y promocionales, y no sustituyen la orientación profesional en temas
+                de salud.
+            </p>
+            <p>
+                Respete las regulaciones locales sobre la venta y consumo de estos productos.
+            </p>
+        </div>
+    </>
+}
