84ea181f395a3e1bd150fc449cf03c30b91a9304
twinkgenocide <illusivelove@proton.me>
2025-10-16 22:58:17 -0300

    Añadido elemento Footer

--- frontend/src/layouts/LayoutPublic.jsx
+++ frontend/src/layouts/LayoutPublic.jsx
@@ -1,8 +1,16 @@
+import { Outlet } from "react-router-dom"
 import { Navbar } from "../components/public/Navbar/Navbar.jsx"
+import { Footer } from "../components/public/Footer/Footer.jsx"
 import "./LayoutPublic.css"
 
 export function LayoutPublic() {
   return <>
-    <Navbar />
+    <div className="layout">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   </>
 }
