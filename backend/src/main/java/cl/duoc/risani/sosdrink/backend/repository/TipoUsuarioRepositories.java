b864125172ff24e882b6bdf880fd97c6b15af30c
furanko222 <f.pisanimontenegro@gmail.com>
2025-10-22 01:38:56 -0300

    Se crean repositorios correspondientes a las entidades Usuario, TipoUsuario, TipoProducto y Producto para habilitar operaciones CRUD  en la base de datos utilizando Spring Data JPA.

---
+++ backend/src/main/java/cl/duoc/risani/sosdrink/backend/repository/TipoUsuarioRepositories.java
@@ -0,0 +1,8 @@
+package cl.duoc.risani.sosdrink.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import cl.duoc.risani.sosdrink.backend.entities.TipoUsuario;
+
+public interface TipoUsuarioRepositories extends CrudRepository<TipoUsuario, Long> {
+
+}
