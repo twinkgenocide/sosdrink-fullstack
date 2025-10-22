97633e1205c253f15141660ef5a6c9279466f983
furanko222 <f.pisanimontenegro@gmail.com>
2025-10-22 01:10:13 -0300

    Se crea cuerpo de identidad Usuario

--- backend/src/main/java/cl/duoc/risani/sosdrink/backend/entities/Usuario.java
+++ backend/src/main/java/cl/duoc/risani/sosdrink/backend/entities/Usuario.java
@@ -1,9 +1,45 @@
 package cl.duoc.risani.sosdrink.backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String tipoUsuario; 
+    private String direccion;
+    private String clave;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Usuario> usuarios;
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", correo=" + correo
+                + ", tipoUsuario=" + tipoUsuario + ", direccion=" + direccion + ", clave=" + clave + "]";
+    }
 
 
+   
 
 }
