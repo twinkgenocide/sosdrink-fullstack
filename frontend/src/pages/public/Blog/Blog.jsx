9ef95856bb882ed78cbe3d6637ed9ce70be958ad
twinkgenocide <illusivelove@proton.me>
2025-10-23 21:43:39 -0300

    cambios a blog (?)

--- frontend/src/pages/public/Blog/Blog.jsx
+++ frontend/src/pages/public/Blog/Blog.jsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Disclaimer } from "../../../components/public/Disclaimer/Disclaimer";
-import "./Blog.css"
 import { Spinner } from "../../../components/public/Spinner/Spinner";
+import "./Blog.css"
 
 function BlogDisplay(props) {
     return <>
