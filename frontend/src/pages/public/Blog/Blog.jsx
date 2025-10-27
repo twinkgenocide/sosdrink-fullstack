fa3ff3b92ca77b2d1949082e5329cd8d4de7dcef
twinkgenocide <illusivelove@proton.me>
2025-10-27 00:24:45 -0300

    Conexión de frontend Blog a API, e implementación de Markdown

--- frontend/src/pages/public/Blog/Blog.jsx
+++ frontend/src/pages/public/Blog/Blog.jsx
@@ -3,18 +3,21 @@ import { useEffect, useState } from "react";
 import { Disclaimer } from "../../../components/public/Disclaimer/Disclaimer";
 import { Spinner } from "../../../components/public/Spinner/Spinner";
 import "./Blog.css"
+import Markdown from "react-markdown";
 
-function BlogDisplay(props) {
+const api_path = (subpath) => `${window.location.protocol}//${window.location.hostname}:8080/${subpath}`
+
+function BlogDisplay({ blog }) {
     return <>
         <article className="blog">
-            <img src="https://talentclick.com/wp-content/uploads/2021/08/placeholder-image.png" />
+            <img src={api_path(blog.imagenUrl)} />
             <header className="blog-header">
-                <h1>{props.title}</h1>
-                <h3>{props.summary}</h3>
+                <h1>{blog.titulo}</h1>
+                <h3>{blog.resumen}</h3>
             </header>
             <hr />
             <section className="blog-content">
-                <p>{props.content}</p>
+                <Markdown>{blog.contenido}</Markdown>
                 <Disclaimer />
             </section>
         </article>
@@ -26,14 +29,14 @@ export function Blog() {
     let [blog, setBlog] = useState(null)
     let [error, setError] = useState(null)
 
-    const url = `https://jsonplaceholder.typicode.com/posts/${params.blogId}`;
+    const url = `${window.location.protocol}//${window.location.hostname}:8080/api/blogs/${params.blogId}`
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(url);
             if (response.ok) {
                 const data = await response.json();
-                setTimeout(() => { setBlog(data) }, 3000)
+                setTimeout(() => { setBlog(data) }, 1000)
             } else {
                 setError(response.status);
             }
@@ -49,7 +52,7 @@ export function Blog() {
     return (
         <div className="blog-container">
             {blog ? (
-                <BlogDisplay title={blog.title} summary="lol" content={blog.body} />
+                <BlogDisplay blog={blog} />
             ) : (
                 <Spinner />
             )}
