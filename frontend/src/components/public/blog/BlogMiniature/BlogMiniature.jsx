140f0d4f37132091e23893eb610fdd09389fc425
twinkgenocide <illusivelove@proton.me>
2025-10-29 13:17:03 -0300

    blog mini style

--- frontend/src/components/public/blog/BlogMiniature/BlogMiniature.jsx
+++ frontend/src/components/public/blog/BlogMiniature/BlogMiniature.jsx
@@ -1,19 +1,53 @@
 import { useEffect, useState } from "react";
 import { api_path } from "../../../../util/apipath"
+import { Spinner } from "../../Spinner/Spinner";
+
 import "./BlogMiniature.css"
+import { Tag } from "../../Tag/Tag";
+import { useNavigate } from "react-router-dom";
 
 export function BlogMiniature({ blog }) {
     const [image, setImage] = useState();
     const url = api_path(blog.imagenUrl);
+    const navigate = useNavigate();
+
+    const fetchImage = async () => {
+        const res = await fetch(url);
+        const blob = await res.blob();
+        setTimeout(() => {
+            setImage(URL.createObjectURL(blob));
+        }, Math.floor(Math.random() * 2000))
+    }
 
     useEffect(() => {
-        const getImage = async () => {
-            // finish body
-        }
+        fetchImage();
     }, []);
 
-    return <div className="blog-miniature">
-        <img src={api_path(blog.imagenUrl)} />
-        <h4>{blog.titulo}</h4>
+    return <div className="blog-miniature" onClick={(e) => {
+        if (e.target.closest("a")) return;
+        navigate(`/blogs/${blog.id}`);
+    }}>
+        {
+            image ?
+                <BlogMiniContent blog={blog} image={image} />
+                : <Spinner />
+        }
+    </div>
+}
+
+function BlogMiniContent({ blog, image }) {
+    return <div className="content">
+        <img src={image} />
+        <div className="meta">
+            <time dateTime={blog.fecha}>{new Date(blog.fecha).toLocaleString('es-CL', {
+                weekday: 'short',
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric'
+            })}</time>
+            <Tag href={`/blogs?categoria=${blog.categoriaBlog.id}`} text={blog.categoriaBlog.nombre} />
+        </div>
+        <h3>{blog.titulo}</h3>
+        <p>{blog.resumen}</p>
     </div>
 }
