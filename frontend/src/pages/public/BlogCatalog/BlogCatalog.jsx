ef03387cedafb6400fdee8162b9edf83dce41707
twinkgenocide <illusivelove@proton.me>
2025-10-29 21:06:32 -0300

    cambios a blogCatalog

--- frontend/src/pages/public/BlogCatalog/BlogCatalog.jsx
+++ frontend/src/pages/public/BlogCatalog/BlogCatalog.jsx
@@ -5,35 +5,53 @@ import { Spinner } from "../../../components/public/Spinner/Spinner";
 import { BlogMiniature } from "../../../components/public/blog/BlogMiniature/BlogMiniature";
 import "./BlogCatalog.css"
 
-export function BlogCatalog() {
+export function BlogCatalogPage() {
     const [searchParams] = useSearchParams();
+    const [categoria, setCategoria] = useState(searchParams.get('categoria') || null);
+
+    useEffect(() => {
+        setCategoria(searchParams.get('categoria') || null);
+    }, [searchParams])
+
+    return <BlogCatalog categoria={categoria} />
+}
+
+export function BlogCatalog({ categoria, vertical, skip, texto }) {
     const [blogs, setBlogs] = useState([]);
+    const [tituloDefault, setTituloDefault] = useState("Sabores, novedades y más")
 
     useEffect(() => {
         setBlogs([]);
-        const cat = searchParams.get("categoria")
-        const url = api_path(cat ? `api/blogs?categoria=${cat}` : 'api/blogs');
+        const url = api_path(categoria ? `api/blogs?categoria=${categoria}` : 'api/blogs');
         const fetchData = async () => {
             const response = await fetch(url);
             if (response.ok) {
                 const data = await response.json();
-                setTimeout(() => { setBlogs(data) }, 2000)
+                setTimeout(() => {
+                    setBlogs(data)
+                    if (categoria && data.length > 0) {
+                        const nombreCategoria = data[0].categoriaBlog.nombre;
+                        setTituloDefault(`Tag: "${nombreCategoria}"`);
+                    }
+                }, 2000)
             }
         }
         fetchData();
-    }, [searchParams]);
+    }, [categoria]);
 
-    return (
-        <div className="blog-catalog">
-            {blogs.length > 0 ? <BlogList blogs={blogs} /> : <Spinner />}
-        </div>
-    )
+    return <div className={vertical ? "blog-catalog vertical" : "blog-catalog"}>
+        {blogs.length > 0 ? <div className="blogs">
+            <h1 className="title">{texto ? texto : tituloDefault}</h1>
+            <BlogList blogs={blogs} skip={skip} />
+        </div> : <Spinner />}
+    </div>
 }
 
-function BlogList({ blogs }) {
-    const content = blogs.map(blog => {
-        return <BlogMiniature key={blog.id} blog={blog} />
-    })
+function BlogList({ blogs, skip }) {
+    if (skip == null) skip = [];
+    const content = blogs
+        .filter(blog => !skip.includes(blog.id))
+        .map(blog => <BlogMiniature key={blog.id} blog={blog} />)
 
     return <>{content}</>
 }
